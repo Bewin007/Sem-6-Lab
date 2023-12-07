56517532396d844b47f38df44314662722c1e051
Bewin007 <bewinfelix@karunya.edu.in>
2023-12-07 14:43:19 +0530

    -added 2nd

--- System Software and Compiler Design/Ex0/1.py
+++ System Software and Compiler Design/Ex0/1.py
@@ -2,7 +2,5 @@ in_path = "E:\\Sem-6-Lab\\System Software and Compiler Design\\Ex0\\test.txt"
 out_path = "E:\\Sem-6-Lab\\System Software and Compiler Design\\Ex0\\out.txt"
 
 f = open(in_path, 'r')
-a=f.read()
-
 x = open(out_path, 'w')
-x.write(a)
+x.write(f.read())
---
+++ System Software and Compiler Design/Ex0/2.py
@@ -0,0 +1,7 @@
+a = ['Bewin','Felix','Apple','elf']
+vowels = ['A','E','I','O','U']
+
+
+for i in a:
+    if i[0].upper() in vowels:
+        print(i)
