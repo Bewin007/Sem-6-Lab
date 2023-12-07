9f1547a2dbbcabcfe42fc09d3824de220494e494
Bewin007 <bewinfelix@karunya.edu.in>
2023-12-07 14:59:58 +0530

    added 3

---
+++ System Software and Compiler Design/Ex0/3.py
@@ -0,0 +1,22 @@
+conv = {
+    "1":"one",
+    "2":"two",
+    "3":"three",
+    "4":"four",
+    "5":"five",
+    "6":"six",
+    "7":"seven",
+    "8":"eight",
+    "9":"nine",
+    "0":"zero"
+}
+num = [1,2,3,4,5,6,7,8,9,0]
+# n = input()
+test_str ="Bewin 1"
+
+
+for ele in test_str:
+    if ele.isdigit():
+        test_str = test_str.replace(ele, conv[ele])
+print(test_str)
+        
