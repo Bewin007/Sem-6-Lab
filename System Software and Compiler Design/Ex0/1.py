99a7e12cb4b24172beeb1d222bd785a72fae5b9f
Bewin007 <bewinfelix@karunya.edu.in>
2023-12-07 14:33:11 +0530

    - added 1

--- System Software and Compiler Design/Ex0/1.py
+++ System Software and Compiler Design/Ex0/1.py
@@ -1,1 +1,9 @@
- 
+in_path = "E:\\Sem-6-Lab\\System Software and Compiler Design\\Ex0\\test.txt"
+out_path = "E:\\Sem-6-Lab\\System Software and Compiler Design\\Ex0\\out.txt"
+# lines = []
+
+f = open(in_path, 'r')
+a=f.read()
+
+x = open(out_path, 'a')
+x.write(a)
